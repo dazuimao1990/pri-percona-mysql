c416b0175b7ee22ceba3a8206d49c53fbeed7f8f
guox <guox@goodrain.com>
2019-05-10 10:58:16 +0800

    format it

--- 5.6/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# do something before mysql start
-## read env and create mysql config file
-/usr/local/bin/env2file create --format mysql --path /etc/mysql/conf.d/custom.cnf
-# start mysql
-exec /usr/local/bin/docker-entrypoint.sh "$@"
